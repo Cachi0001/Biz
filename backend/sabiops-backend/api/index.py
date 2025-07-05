286575a5f5219c8b2a42c8af6e3972a9c7dcd529
Cachi001 <manus@example.com>
2025-07-05 11:27:10 -0400

    Add global after_request hook for CORS headers

--- backend/sabiops-backend/api/index.py
+++ backend/sabiops-backend/api/index.py
@@ -148,3 +148,14 @@ def handle_options_requests():
         return response
 
 
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    response.headers.add("Access-Control-Allow-Credentials", "true")
+    return response
+
+
