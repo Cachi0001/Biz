af0024028a3968258069b834acbe80ab34484f2e
Cachi0001 <passioncaleb5@gmail.com>
2025-07-15 14:24:42 +0100

    Fixing build error issues 3

--- frontend/sabiops-frontend/src/lib/utils/index.js
+++ frontend/sabiops-frontend/src/lib/utils/index.js
@@ -5,3 +5,34 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
 
+export function formatCurrency(amount, currency = 'NGN') {
+  return new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount)
+}
+
+export function formatNumber(number) {
+  return new Intl.NumberFormat('en-NG').format(number)
+}
+
+export function formatDate(date) {
+  return new Intl.DateTimeFormat('en-NG', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }).format(new Date(date))
+}
+
+export function formatDateTime(date) {
+  return new Intl.DateTimeFormat('en-NG', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date))
+}
+
